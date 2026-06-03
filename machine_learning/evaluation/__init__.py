a91b640feba1de2c81dba4cfcbfebcfc982fc132
mburuxx <njihiamburu.3@gmail.com>
2026-06-03 10:17:06 +0300

    chore: stop tracking local machine_learning package
    Remove ML package, notebook, and export script from git while keeping files locally. Add gitignore entries so they are not pushed again.

--- machine_learning/evaluation/__init__.py
+++
@@ -1,9 +0,0 @@
-from machine_learning.evaluation.metrics import classification_metrics, trading_filter_metrics
-from machine_learning.evaluation.walk_forward import WalkForwardResult, walk_forward_evaluate
-
-__all__ = [
-    "classification_metrics",
-    "trading_filter_metrics",
-    "WalkForwardResult",
-    "walk_forward_evaluate",
-]
--- machine_learning/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from machine_learning.models.train import TrainedModel, train_classifier
-from machine_learning.models.predict import load_model, predict_trades
-
-__all__ = ["TrainedModel", "train_classifier", "load_model", "predict_trades"]
